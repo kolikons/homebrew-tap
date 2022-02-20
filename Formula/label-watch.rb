ae567b1669626443e2877d2eb3e35353a432011a
goreleaserbot <goreleaser@carlosbecker.com>
2022-02-20 23:33:59 +0200

    Brew formula update for label-watch version v0.0.2-rc2

--- Formula/label-watch.rb
+++ Formula/label-watch.rb
@@ -5,20 +5,20 @@
 class LabelWatch < Formula
   desc "label-watch checks a specific label on worker node then create an label"
   homepage "https://github.com/kolikons/label-watch"
-  version "0.0.2-rc1"
+  version "0.0.2-rc2"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/kolikons/label-watch/releases/download/v0.0.2-rc1/label-watch_0.0.2-rc1_darwin_amd64.tar.gz"
-      sha256 "05598a1b44f8c9c9e31426350a9920ea9bd91786b4328e83664617db25a9e6d2"
+      url "https://github.com/kolikons/label-watch/releases/download/v0.0.2-rc2/label-watch_0.0.2-rc2_darwin_amd64.tar.gz"
+      sha256 "52f98312c90fe9295295f5d4a1b46747d3dfb8c456fde86c8e65bf2decdff10a"
 
       def install
         bin.install "label-watch"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/kolikons/label-watch/releases/download/v0.0.2-rc1/label-watch_0.0.2-rc1_darwin_arm64.tar.gz"
-      sha256 "5afc915f15ccc7ad1bcb0fca25c06f3bc799d13b2ae21c39ae42d732360008ac"
+      url "https://github.com/kolikons/label-watch/releases/download/v0.0.2-rc2/label-watch_0.0.2-rc2_darwin_arm64.tar.gz"
+      sha256 "af7da11023b1bf024252706dcb0ed39836c04304b4a5ee807b622a99ab7f58c8"
 
       def install
         bin.install "label-watch"
@@ -28,8 +28,8 @@ class LabelWatch < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/kolikons/label-watch/releases/download/v0.0.2-rc1/label-watch_0.0.2-rc1_linux_amd64.tar.gz"
-      sha256 "e3daca21e006c5b75ec2587e01bc9185167550aab4ddfebfb4e92dfacfd1a0bc"
+      url "https://github.com/kolikons/label-watch/releases/download/v0.0.2-rc2/label-watch_0.0.2-rc2_linux_amd64.tar.gz"
+      sha256 "fce6c59dea749ec9e02b49baa6de3b6f3dd3964632cb696832e8c018c74d31cf"
 
       def install
         bin.install "label-watch"
